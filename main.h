6cf55963deb8e85325162bac3fb88241e88f61b2
Pavel korotkevich <pawday@mail.ru>
2023-07-05 17:13:09 +0300

    Change stream source

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include "mpegts/packet.h"
 #include "mpegts/pmt_builder.h"
 
-#define TS_FILE_URL "http://home.imaqliq.tv/Katy8.ts"
+#define TS_FILE_URL "http://93.189.151.113:8754/udp/234.166.172.91:5555"
 
 typedef struct
 {
