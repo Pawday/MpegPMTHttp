8d26fababc14b59681d4b01452f2e75fbc3808db
Pavel korotkevich <pawday@mail.ru>
2023-07-06 12:08:34 +0300

    Make single exit point

--- main.c
+++ main.c
@@ -5,24 +5,24 @@
 
 #include "main.h"
 #include "mpegts/packet_inplace_parser.h"
+#include "mpegts/pmt_builder.h"
 #include "mpegts/pmt_dumper.h"
 
-void build_table_print_it_then_exit(MpegTsPMTBuilder_t *builder)
+bool build_and_print_pmt(MpegTsPMTBuilder_t *builder)
 {
 
     if (builder->state != PMT_BUILDER_STATE_TABLE_ASSEMBLED) {
-        exit(EXIT_FAILURE);
+        return false;
     }
 
     MpegTsPMT_t table = {0};
     if (!mpeg_ts_pmt_builder_try_build_table(builder, &table)) {
-        exit(EXIT_FAILURE);
+        return false;
     }
 
     mpeg_ts_dump_pmt_to_stream(&table, stdout);
     printf("\n");
-
-    exit(EXIT_SUCCESS);
+    return true;
 }
 
 size_t curl_data_chunk_recv_handler(void *data, size_t size, size_t nmemb, PMTParseKit_t *parse_kit)
@@ -34,12 +34,22 @@ size_t curl_data_chunk_recv_handler(void *data, size_t size, size_t nmemb, PMTPa
         parse_kit->packets,
         sizeof(parse_kit->packets) / sizeof(parse_kit->packets[0]));
 
-    for (size_t packet_index = 0; packet_index < parsed_packets; packet_index++) {
+    bool pmt_was_found = false;
+    bool error_occurred = false;
+
+    for (size_t packet_index = 0;
+         !pmt_was_found && !error_occurred && packet_index < parsed_packets;
+         packet_index++) {
         switch (mpeg_ts_pmt_builder_try_send_packet(&parse_kit->pmt_builder,
             parse_kit->packets + packet_index)) {
         case PMT_BUILDER_SEND_STATUS_TABLE_IS_ASSEMBLED:
         case PMT_BUILDER_SEND_STATUS_REDUNDANT_PACKET_REJECTED:
-            build_table_print_it_then_exit(&parse_kit->pmt_builder);
+            if (build_and_print_pmt(&parse_kit->pmt_builder)) {
+                pmt_was_found = true;
+                break;
+            }
+            mpeg_ts_pmt_builder_reset(&parse_kit->pmt_builder);
+            continue;
         case PMT_BUILDER_SEND_STATUS_NEED_MORE_PACKETS:
         case PMT_BUILDER_SEND_STATUS_INVALID_PACKET_REJECTED:
             continue;
@@ -47,9 +57,13 @@ size_t curl_data_chunk_recv_handler(void *data, size_t size, size_t nmemb, PMTPa
             mpeg_ts_pmt_builder_reset(&parse_kit->pmt_builder);
             continue;
         case PMT_BUILDER_SEND_STATUS_NOT_ENOUGHT_MEMORY:
-            exit(EXIT_FAILURE);
+            error_occurred = true;
             break;
         }
+    }
+
+    if (error_occurred || pmt_was_found) {
+        return 0;
     }
 
     return nmemb;
@@ -57,7 +71,6 @@ size_t curl_data_chunk_recv_handler(void *data, size_t size, size_t nmemb, PMTPa
 
 int main()
 {
-    int exit_code = EXIT_SUCCESS;
     CURL *curl = curl_easy_init();
     PMTParseKit_t parse_kit;
 
@@ -66,11 +79,11 @@ int main()
 
     if (!curl) {
         printf("Error setup curl\n");
-        goto exit_after_close_curl;
+        return EXIT_FAILURE;
     }
 
-    if (curl_easy_setopt(curl, CURLOPT_URL, TS_FILE_URL) != CURLE_OK) {
-        printf("Error set %s addres\n", TS_FILE_URL);
+    if (curl_easy_setopt(curl, CURLOPT_URL, TS_STREAM_URL) != CURLE_OK) {
+        printf("Error set %s address\n", TS_STREAM_URL);
         goto error_exit_clean_curl;
     }
 
@@ -83,23 +96,24 @@ int main()
         goto error_exit_clean_curl;
     }
 
-    if (curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, MPEG_TS_PACKET_SIZE * 5) != CURLE_OK) {
+    if (curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, CURL_RECV_BUFFER_SIZE) != CURLE_OK) {
         printf("Error set curl buffer size\n");
         goto error_exit_clean_curl;
     }
 
     CURLcode perform_status = curl_easy_perform(curl);
-    if (perform_status != CURLE_OK) {
+
+    // In that case CURLE_WRITE_ERROR means "we are done and you can terminate"
+    // or "pmt builder no memory"
+    if (perform_status != CURLE_WRITE_ERROR) {
         printf("%s\n", curl_easy_strerror(perform_status));
         goto error_exit_clean_curl;
     }
 
-    goto exit_clean_curl;
+    curl_easy_cleanup(curl);
+    return EXIT_SUCCESS;
 
 error_exit_clean_curl:
-    exit_code = EXIT_FAILURE;
-exit_clean_curl:
     curl_easy_cleanup(curl);
-exit_after_close_curl:
-    return exit_code;
+    return EXIT_FAILURE;
 }
